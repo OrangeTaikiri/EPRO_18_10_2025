9ed179e5a657f023a24abf9418ebfcd374b605c3
orangetaikiri <Rafche@gmail.com>
2025-10-18 20:44:03 +0200

    add computer mode

--- src/main/java/org/lecture/Game.java
+++ src/main/java/org/lecture/Game.java
@@ -10,7 +10,8 @@ public class Game {
         Move player1 = Move.SCISSORS;
         Move player2 = Move.PAPER;
 
-        System.out.println( evaluateMove(player1, player2));
+        // Player 1 vs. Player 2
+        System.out.println( evaluateMove(player1, player2, "Player 2"));
 
         Move computer = switch (ThreadLocalRandom.current().nextInt(1, 4)) {
             case 1 -> Move.ROCK;
@@ -20,13 +21,14 @@ public class Game {
                     throw new IllegalStateException("Unexpected value: " + ThreadLocalRandom.current().nextInt(1, 4));
         };
 
-        System.out.println("computer = " + evaluateMove(player1, computer));
+        // Player 1 vs. Computer
+        System.out.println(evaluateMove(player1, computer, "Computer"));
 
 
     }
 
     // Returns the winner of the game
-    private static String evaluateMove(Move player1, Move player2) {
+    private static String evaluateMove(Move player1, Move player2, String competitor) {
 
         // Tie
         if (player1.equals(player2)) {
@@ -38,11 +40,11 @@ public class Game {
             player1PaperBeatsRock(player1, player2)    ||
             player1ScissorBeatsPaper(player1, player2)
         ) {
-            return "Player 1 wins";
+            return "Player 1 wins" + " | " + player1.name() + " vs. " + player2.name();
         }
 
-        // Player 2 wins if no condition above is met
-        return "Player 2 wins";
+        // Player 2 or Computer wins if no condition above is met
+        return competitor + " wins | " + player2.name() + " vs. " + player1.name();
     }
 
     private static boolean player1ScissorBeatsPaper(Move player1, Move player2) {
